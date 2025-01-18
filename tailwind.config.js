fe27b24dec0d3ed3796df2a99448facb03f6f690
CHW <81775007+chrwiencke@users.noreply.github.com>
2025-01-18 23:20:49 +0100

    refactor: remove cross-env dependency and simplify CSS build script

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,6 @@ module.exports = {
     "./views/**/*.ejs",
     "./public/**/*.{js,css}"
   ],
-  safelist: [
-    'bg-yellow-100',
-    'border-l-4',
-    'border-yellow-500',
-    'text-yellow-700',
-    'invisible',
-    'group-hover:visible',
-    'bg-gray-700',
-    'after:content-[\'\']',
-    'after:absolute',
-    'after:border-8',
-    'after:border-x-transparent',
-    'after:border-t-transparent',
-    'after:border-b-gray-700'
-  ],
   theme: {
     extend: {},
   },
