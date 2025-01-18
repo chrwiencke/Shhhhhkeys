7d7815061f0d0bc96e5d35852aca4dc728c6cf10
CHW <81775007+chrwiencke@users.noreply.github.com>
2025-01-18 23:19:17 +0100

    feat: update Tailwind CSS configuration to include additional content paths and safelist classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/*.ejs",
-    "./views/dashboard/*.ejs",
-    "./views/partials/*.ejs"
+    "./views/**/*.ejs",
+    "./public/**/*.{js,css}"
+  ],
+  safelist: [
+    'bg-yellow-100',
+    'border-l-4',
+    'border-yellow-500',
+    'text-yellow-700',
+    'invisible',
+    'group-hover:visible',
+    'bg-gray-700',
+    'after:content-[\'\']',
+    'after:absolute',
+    'after:border-8',
+    'after:border-x-transparent',
+    'after:border-t-transparent',
+    'after:border-b-gray-700'
   ],
   theme: {
     extend: {},
